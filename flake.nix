39337608442728ecaffc846848482fcf6c63e525
Steven Ulin <steveulin@gmail.com>
2024-05-04 15:37:24 -0700

    Create a hello world file using c++23's print statement

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "C++ Libary & Examples";
+
+  inputs = {
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      in {
+        devShells.default = pkgs.mkShell.override {
+            stdenv = pkgs.llvmPackages_18.libcxxStdenv;
+        } {
+          name = "Tempura";
+
+          packages = with pkgs; [
+            cmake
+            ninja
+          ];
+
+          shellHook = ''
+            export CMAKE_GENERATOR="Ninja";
+          '';
+        };
+      }
+    );
+}
