a786e6ff076e0b6db8689050807adbdb49a53b00
Steven Ulin <steveulin@gmail.com>
2024-11-23 13:04:28 -0800

    Add Col Permuted

--- src/matrix2/views/permuted.h
+++ src/matrix2/views/permuted.h
@@ -58,4 +58,48 @@ template <typename MatT>
 RowPermuted(MatT&&, Permutation<std::remove_cvref_t<MatT>::kRow>)
     -> RowPermuted<MatT>;
 
+// ColPermuted is a wrapper around a matrix that lets you "swap columns"
+// without moving the data in memory.
+template <typename MatT>
+class ColPermuted {
+ public:
+  using ValueType = typename std::remove_cvref_t<MatT>::ValueType;
+  static constexpr int64_t kRow = std::remove_cvref_t<MatT>::kRow;
+  static constexpr int64_t kCol = std::remove_cvref_t<MatT>::kCol;
+
+  constexpr ColPermuted(MatT&& mat) : mat_(std::forward<MatT>(mat)) {}
+
+  constexpr ColPermuted(MatT&& mat, Permutation<kCol> perm)
+      : mat_(std::forward<MatT>(mat)), perm_(std::move(perm)) {}
+
+  constexpr auto operator[](this auto&& self, int64_t i, int64_t j)
+      -> decltype(auto) {
+    return self.mat_[i, self.perm_.data()[j]];
+  }
+
+  constexpr auto shape() const -> RowCol { return mat_.shape(); }
+  constexpr auto swap(int64_t i, int64_t j) {
+    if (std::is_constant_evaluated()) {
+      CHECK(i < mat_.shape().col);
+      CHECK(j < mat_.shape().col);
+    }
+    perm_.swap(i, j);
+  }
+
+  constexpr auto permutation() const -> const Permutation<kCol>& {
+    return perm_;
+  }
+
+ private:
+  MatT mat_;
+  Permutation<kCol> perm_ = {};
+};
+
+template <typename MatT>
+ColPermuted(MatT&&) -> ColPermuted<MatT>;
+
+template <typename MatT>
+ColPermuted(MatT&&, Permutation<std::remove_cvref_t<MatT>::kCol>)
+    -> ColPermuted<MatT>;
+
 }  // namespace tempura::matrix
--- src/matrix2/views/permuted_test.cpp
+++ src/matrix2/views/permuted_test.cpp
@@ -9,10 +9,10 @@ using namespace tempura;
 auto main() -> int {
   "RowPermuted - Default Constructor"_test = [] {
     static constexpr matrix::InlineDense d{
-        {1, 0, 0, 0},
-        {0, 1, 0, 0},
-        {0, 0, 1, 0},
-        {0, 0, 0, 1},
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16},
     };
     constexpr matrix::RowPermuted r{d};
     static_assert(r == d);
@@ -20,29 +20,29 @@ auto main() -> int {
 
   "RowPermuted - Permutation Constructor"_test = [] {
     static constexpr matrix::InlineDense d{
-        {1, 0, 0, 0},
-        {0, 1, 0, 0},
-        {0, 0, 1, 0},
-        {0, 0, 0, 1},
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16},
     };
     constexpr matrix::RowPermuted r{
         d, matrix::Permutation<(int64_t)4>{3, 2, 1, 0}};
 
     constexpr matrix::InlineDense expected{
-        {0, 0, 0, 1},
-        {0, 0, 1, 0},
-        {0, 1, 0, 0},
-        {1, 0, 0, 0},
+        {13, 14, 15, 16},
+        {9, 10, 11, 12},
+        {5, 6, 7, 8},
+        {1, 2, 3, 4},
     };
     static_assert(r == expected);
   };
 
   "RowPermuted - Swap"_test = [] {
     static constexpr matrix::InlineDense d{
-        {1, 0, 0, 0},
-        {0, 1, 0, 0},
-        {0, 0, 1, 0},
-        {0, 0, 0, 1},
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16},
     };
     constexpr matrix::RowPermuted r = [] {
       matrix::RowPermuted r{d};
@@ -50,11 +50,85 @@ auto main() -> int {
       r.swap(2, 3);
       return r;
     }();
-    static_assert(r == matrix::InlineDense{
-                           {0, 1, 0, 0},
-                           {1, 0, 0, 0},
-                           {0, 0, 0, 1},
-                           {0, 0, 1, 0},
-                       });
+    constexpr matrix::InlineDense expected{
+        {5, 6, 7, 8},
+        {1, 2, 3, 4},
+        {13, 14, 15, 16},
+        {9, 10, 11, 12},
+    };
+    static_assert(r == expected);
+  };
+
+  "ColPermuted - Default Constructor"_test = [] {
+    static constexpr matrix::InlineDense d{
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16},
+    };
+    constexpr matrix::ColPermuted c{d};
+    static_assert(c == d);
+  };
+
+  "ColPermuted - Swap"_test = [] {
+    static constexpr matrix::InlineDense d{
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16},
+    };
+    constexpr matrix::ColPermuted c = [] {
+      matrix::ColPermuted c{d};
+      c.swap(0, 1);
+      c.swap(2, 3);
+      return c;
+    }();
+    constexpr matrix::InlineDense expected{
+        {2, 1, 4, 3},
+        {6, 5, 8, 7},
+        {10, 9, 12, 11},
+        {14, 13, 16, 15},
+    };
+    static_assert(c == expected);
+  };
+
+  "ColPermuted - Permutation Constructor"_test = [] {
+    static constexpr matrix::InlineDense d{
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16},
+    };
+    constexpr matrix::ColPermuted c{
+        d, matrix::Permutation<(int64_t)4>{3, 2, 1, 0}};
+    constexpr matrix::InlineDense expected{
+        {4, 3, 2, 1},
+        {8, 7, 6, 5},
+        {12, 11, 10, 9},
+        {16, 15, 14, 13},
+    };
+    static_assert(c == expected);
+  };
+
+  "ColPermuted - Swap"_test = [] {
+    static constexpr matrix::InlineDense d{
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16},
+    };
+    constexpr matrix::ColPermuted c = [] {
+      matrix::ColPermuted c{d};
+      c.swap(0, 1);
+      c.swap(2, 3);
+      return c;
+    }();
+    constexpr matrix::InlineDense expected{
+        {2, 1, 4, 3},
+        {6, 5, 8, 7},
+        {10, 9, 12, 11},
+        {14, 13, 16, 15},
+    };
+    static_assert(c == expected);
   };
 }
