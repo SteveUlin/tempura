d4c521bf5c7745c0b9219a8555f6992cbbc8b1eb
Steven Ulin <steveulin@gmail.com>
2025-02-02 15:50:01 -0800

    Quadature

---
+++ src/quadature/newton_cotes.h
@@ -0,0 +1,157 @@
+#pragma once
+
+#include <cassert>
+#include <concepts>
+#include <cstdint>
+#include <deque>
+#include <utility>
+#include <vector>
+
+#include "interpolate.h"
+
+// Newton-Cotes quadature are numerical integration methods that approximate
+// an integral by evaluating the integrand at equally spaced points.
+//
+// https://en.wikipedia.org/wiki/Newton%E2%80%93Cotes_formulas
+
+namespace tempura::quadature {
+
+// Trapezoidal Integrator approximates the integral of a function by evaluating
+// the function at a bunch of points, then connecting the points with straight
+// lines.
+template <typename T, std::invocable<T> F, typename U = std::invoke_result_t<F, T>>
+class TrapazoidalIntegrator {
+ public:
+  // ThapazoidalIntegrator will always evaluate the function at the endpoints,
+  // You make optionally specify the number of points to evaluate the function
+  // on the interval [a, b].
+  constexpr TrapazoidalIntegrator(F func, T a, T b, int64_t initial_points = 0)
+      : func_{std::move(func)},
+        a_{std::move(a)},
+        b_{std::move(b)},
+        num_points_{initial_points},
+        result_{(func_(a_) + func_(b_)) / 2.0} {
+    assert(num_points_ >= 0);
+    // Each endpoint counts as half a point
+    ++num_points_;
+    const U weight = U{1.0} / num_points_;
+    result_ *= weight;
+
+    const T delta = (b_ - a_) / num_points_;
+    T curr = a_ + delta;
+    // We already evaluated the function at the endpoints so this loop
+    // is num_points - 1.
+    for (int64_t i = 0; i < num_points_ - 1; ++i) {
+      result_ += func_(curr) * weight;
+      curr += delta;
+    }
+  }
+
+  constexpr auto result() const -> U { return result_ * (b_ - a_); }
+
+  constexpr auto refine() {
+    result_ /= 2.0;
+
+    const T delta = (b_ - a_) / num_points_;
+    const U weight = U{1.0} / (num_points_ * 2);
+    T curr = a_ + (delta / 2.0);
+
+    for (int64_t i = 0; i < num_points_; ++i) {
+      result_ += func_(curr) * weight;
+      curr += delta;
+    }
+
+    num_points_ *= 2;
+  }
+
+ private:
+  F func_;
+  T a_;
+  T b_;
+  int64_t num_points_;
+  U result_;
+};
+
+template <typename T, std::invocable<T> F>
+TrapazoidalIntegrator(F, T, T, int64_t) -> TrapazoidalIntegrator<T, F>;
+
+// Simpson's Rule is a quadrature method that approximates the integral of a
+// function with a quadratic polynomial. The error of Simpson's Rule is one
+// degree higher than the error of the Trapezoidal Rule for the same number of
+// function evaluations.
+//
+// If Sₙ is the trapezoidal rule and S₂ₙ is Simpson's Rule, then:
+// Simpson == 4/3 S₂ₙ - 1/3 Sₙ
+//
+// See Numerical Recipes, 3rd Edition, Section 4.2
+
+template <typename T, std::invocable<T> F, typename U = std::invoke_result_t<F, T>>
+class SimpsonIntegrator {
+ public:
+  // num_points is the initial number of interior evaluation points of the
+  // function divided by two since we apply the TrapezoidalIntegrator twice.
+  constexpr SimpsonIntegrator(F func, T a, T b, int64_t initial_points = 0)
+      : trapazoidal_{std::move(func), a, b, initial_points} {
+    prev_ = trapazoidal_.result();
+    trapazoidal_.refine();
+    curr_ = trapazoidal_.result();
+  }
+
+  constexpr auto result() const -> U { return (4.0 * curr_ - prev_) / 3.0; }
+
+  constexpr auto refine() {
+    prev_ = curr_;
+    trapazoidal_.refine();
+    curr_ = trapazoidal_.result();
+  }
+
+ private:
+  TrapazoidalIntegrator<T, F, U> trapazoidal_;
+  U prev_;
+  U curr_;
+};
+
+template <typename T, std::invocable<T> F>
+SimpsonIntegrator(F, T, T, int64_t) -> SimpsonIntegrator<T, F>;
+
+// Romberg's Method evaluates the integral at different levels of fidelity.
+// We then fit a polynomial to the results and extrapolate to the limit of
+// h -> 0.
+//
+// We only fit a polynomial to the last K results. (Simpson's Rule can be
+// viewed as a special case of Romberg's Method where K = 2).
+//
+// The error of the Trapezoidal Rule only includes even powers of h, so we
+// interpolate in terms of h² instead of h.
+//
+// See Numerical Recipes, 3rd Edition, Section 4.2
+template <typename T, std::invocable<T> F, typename U = std::invoke_result_t<F, T>>
+class RombergIntegrator {
+ public:
+  constexpr RombergIntegrator(int64_t levels, F func, T a, T b, int64_t initial_points = 0)
+      : levels_{levels}, trapazoidal_{std::move(func), a, b, initial_points} {
+    results_.emplace_back(1.0, trapazoidal_.result());
+    for (int64_t i = 1; i < levels; ++i) {
+      trapazoidal_.refine();
+      // The x coordinate corresponds to a normalized h²
+      results_.emplace_back(results_.back().first * 0.25, trapazoidal_.result());
+    }
+  }
+
+  constexpr auto result() const -> U {
+    return PolynomialInterpolator{std::views::all(results_)}(0.0);
+  }
+
+  constexpr auto refine() {
+    trapazoidal_.refine();
+    results_.pop_front();
+    results_.emplace_back(results_.back().first * 0.25, trapazoidal_.result());
+  }
+
+ private:
+  int64_t levels_;
+  TrapazoidalIntegrator<T, F, U> trapazoidal_;
+  std::deque<std::pair<double, U>> results_;
+};
+
+}  // namespace tempura::quadature
---
+++ test/quadature/newton_cotes_test.cpp
@@ -0,0 +1,114 @@
+#include "quadature/newton_cotes.h"
+
+#include <print>
+#include <random>
+#include <utility>
+
+#include "unit.h"
+
+using namespace tempura;
+using namespace tempura::quadature;
+
+struct Integrand {
+  std::function<double(double)> func;
+  double a;
+  double b;
+  double ans;
+};
+
+auto genPolynomial(std::uniform_random_bit_generator auto& rng) -> Integrand {
+  const double a = std::uniform_real_distribution{-4.0, 4.0}(rng);
+  const double b = std::uniform_real_distribution{-4.0, 4.0}(rng);
+  double ans = 0;
+
+  size_t length = std::uniform_int_distribution{1, 8}(rng);
+  std::vector<std::pair<double, double>> data;
+  data.reserve(length);
+  for (size_t i = 0; i < length; ++i) {
+    const int64_t power = std::uniform_int_distribution{0, 4}(rng);
+    const double coeff = std::uniform_real_distribution{-4.0, 4.0}(rng);
+    data.emplace_back(coeff, power);
+    ans += coeff * (std::pow(b, power + 1) - std::pow(a, power + 1)) / (power + 1);
+  }
+
+  return Integrand{
+      .func =
+          [data](double x) {
+            double result = 0;
+            for (const auto& [coeff, power] : data) {
+              result += coeff * std::pow(x, power);
+            }
+            return result;
+          },
+      .a = a,
+      .b = b,
+      .ans = ans,
+  };
+}
+
+template <typename Integrator, typename... Args>
+void fuzzTest(Args&&... args) {
+  std::default_random_engine rng(1337);
+
+  for (size_t i = 0; i < 1'000; ++i) {
+    auto [func, a, b, ans] = genPolynomial(rng);
+    Integrator integrator{args..., std::move(func), a, b};
+    double result = integrator.result();
+    for (size_t j = 0; j < 20; ++j) {
+      integrator.refine();
+      const double next = integrator.result();
+      const double diff = std::abs((next - result) / next);
+      result = next;
+      if (diff < 1e-6) {
+        break;
+      }
+    }
+    expectLessThan(std::abs((result - ans) / ans), 1e-5);
+  }
+};
+
+// Returns the number of calls to .refine that it took to converges.
+template <typename Integrator>
+auto testConvergence(Integrator integrator) -> int64_t {
+  int64_t calls = 0;
+  auto result = integrator.result();
+  for (int64_t i = 0; i < 30; ++i) {
+    ++calls;
+    integrator.refine();
+    const auto next = integrator.result();
+    const auto diff = std::abs((next - result) / next);
+    result = next;
+    if (diff < 1e-10) {
+      std::println("{}", result);
+      break;
+    }
+  }
+  return calls;
+}
+
+auto main() -> int {
+  "fuzz trap"_test = [] {
+    fuzzTest<TrapazoidalIntegrator<double, std::function<double(double)>>>();
+  };
+
+  "fuzz simpson"_test = [] {
+    fuzzTest<SimpsonIntegrator<double, std::function<double(double)>>>();
+  };
+
+  "fuzz romberg"_test = [] {
+    fuzzTest<RombergIntegrator<double, std::function<double(double)>>>(5);
+  };
+
+  "convergence"_test = [] {
+    auto func = [](double x) {
+      if (x == 0.0) {
+        return 0.0;
+      }
+      return std::pow(x, 4) * std::log(x + std::sqrt((x * x) + 1));
+    };
+    std::println("Trapazoidal: {}", 1 + testConvergence(TrapazoidalIntegrator{func, 0.0, 2.0}));
+    std::println("Simpson: {}", 2 + testConvergence(SimpsonIntegrator{func, 0.0, 2.0}));
+    std::println("Romberg: {}", 5 + testConvergence(RombergIntegrator{5, func, 0.0, 2.0}));
+  };
+  return TestRegistry::result();
+}
