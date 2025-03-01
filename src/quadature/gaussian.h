cb5a2f53fc709ea86dd819d1983478315b55c049
Steven Ulin <steveulin@gmail.com>
2025-02-28 22:22:45 -0800

    Gauss Legendre

--- src/quadature/gaussian.h
+++ src/quadature/gaussian.h
@@ -47,20 +47,20 @@ constexpr auto gaussLegendre(const T a, const T b, const int64_t N,
                              const T ϵ = 1e-14)
     -> std::vector<GaussianWeight<T>> {
   std::vector<GaussianWeight<T>> weights(N);
+  const T xm = 0.5 * (b + a);
+  const T xl = 0.5 * (b - a);
 
   // Solutions are symmetric around the midpoint so we only need to find half
   using std::abs;
-  T prev;
-  T deriv;
   for (int64_t i = 0; i < (N + 1) / 2; ++i) {
-    using std::cos;
     constexpr auto π = std::numbers::pi_v<T>;
+    using std::cos;
     auto z = cos(π * (static_cast<T>(i) + 0.75) / (static_cast<T>(N) + 0.5));
-    do {
+    while (true) {
       T p0 = 0.0;
       T p1 = 1.0;
       // Recurrence relation:
-      // (j + 1) * Pⱼ₊₁  = (2j + 1) * z * Pⱼ - j * Pⱼ₋₁
+      // (j + 1) Pⱼ₊₁  = (2j + 1) z * Pⱼ - j Pⱼ₋₁
       for (int64_t j = 0; j < N; ++j) {
         std::tie(p0, p1) =
             std::pair{p1, ((2.0 * static_cast<T>(j) + 1.0) * z * p1 -
@@ -68,22 +68,70 @@ constexpr auto gaussLegendre(const T a, const T b, const int64_t N,
                               (static_cast<T>(j) + 1.0)};
       }
       // Derivative of Legendre polynomial
-      deriv = N * (z * p1 - p0) / (z * z - 1.0);
+      const T deriv = N * (z * p1 - p0) / (z * z - 1.0);
       // Newton's method
-      prev = z;
+      const T prev = z;
       z -= p1 / deriv;
-    } while (abs(z - prev) > ϵ);
-
-    const T xm = 0.5 * (b + a);
-    const T xl = 0.5 * (b - a);
-    // Special case weight calculation 2 / (1 - z^2) * (Pₙ'(z))^2
-    weights[i] = {.abscissa = xm - (xl * z),
-                  .weight = 2.0 * xl / ((1.0 - z * z) * deriv * deriv)};
-    weights[N - i - 1] = {.abscissa = xm + (xl * z),
-                          .weight = weights[i].weight};
+      if (abs(z - prev) > ϵ) {
+        // Special case weight calculation 2 / (1 - z^2) * (Pₙ'(z))^2
+        weights[i] = {.abscissa = xm - (xl * z),
+                      .weight = 2.0 * xl / ((1.0 - z * z) * deriv * deriv)};
+        weights[N - i - 1] = {.abscissa = xm + (xl * z),
+                              .weight = weights[i].weight};
+        break;
+      }
+    }
   }
 
   return weights;
+}
+
+// Gauss-Laguerre Quadrature
+// ⌠∞
+// ⎮ xᵅ e⁻ˣ dx ≈ ∑ w_i f(x_i)
+// ⌡0
+template <typename T = double, int64_t MAX_ITER = 10>
+constexpr auto gaussLegendre(const T α, const int64_t N, const T ϵ = 1e-14) {
+  std::vector<GaussianWeight<T>> weights(N);
+  for (int64_t i = 0; i < N; ++i) {
+    T z;
+    if (i == 0) {
+      z = (1.0 + α) * (3.0 + 0.92 * α) /
+          (1.0 + 2.4 * static_cast<T>(N) + 1.8 * α);
+    } else if (i == 1) {
+      z += (15.0 + 6.25 * α) / (1.0 + 0.9 * α + 2.5 * static_cast<T>(N));
+    } else {
+      const T ai = static_cast<T>(i - 1);
+      z += ((1.0 + 2.55 * ai) / (1.9 * ai) + 1.26 * ai * α / (1.0 + 3.5 * ai)) *
+           (z - weights[i - 2].abscissa) / (1.0 + 0.3 * α);
+    }
+
+    for (int64_t j = 0; j < MAX_ITER; ++j) {
+      T p0 = 0.0;
+      T p1 = 1.0;
+      // Recurrence relation:
+      // (i + 1) Lᵅⱼ₊₁  = (-z + 2i + α + 1) Lᵅⱼ - (i + α) Lᵅⱼ₋₁
+      for (int64_t k = 0; k < N; ++k) {
+        std::tie(p0, p1) =
+            std::pair{p1, ((2.0 * static_cast<T>(k) + 1.0 + α - z) * p1 -
+                           (statc_cast<T>(k) + α) * p0) /
+                              (static_cast<T>(k) + 1.0)};
+      }
+      T deriv = (N * p1 - (N + α) * p0) / z;
+      const T prev = z;
+      // Newton's method
+      z = prev - p1 / deriv;
+      if (abs(z - prev) < ϵ) {
+        using std::exp;
+        using std::lgamma;
+        weights[i] = {
+            .abscissa = z,
+            .weight = -exp(lgamma(α + N) - lgamma(static_cast<T>(N)) - z) /
+                      (deriv * static_cast<T>(N) * p0)};
+        break;
+      }
+    }
+  }
 }
 
 }  // namespace tempura::quadature
