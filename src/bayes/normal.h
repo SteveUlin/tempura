88f95b7777342125408b7f4a38f447cc27ebe7c1
Steven Ulin <steveulin@gmail.com>
2025-03-09 19:31:17 -0700

    cleanup normal.h a bit

--- src/bayes/normal.h
+++ src/bayes/normal.h
@@ -27,27 +27,35 @@ class Normal {
     T u1 = static_cast<T>(bits) / delta;
     T u2 = static_cast<T>(g()) / delta;
 
-    const T r = sigma_ * sqrt(-2 * log(u1));
-    const T theta = 2 * std::numbers::pi * u2;
+    using std::cos;
+    using std::log;
+    using std::sin;
+    using std::sqrt;
+    const T r = sigma_ * sqrt(-2. * log(u1));
+    const T theta = 2. * std::numbers::pi * u2;
 
     cached_value_ += sigma_ * r * cos(theta);
     has_value_ = true;
 
-    return mu_ + r * sin(theta);
+    return mu_ + (r * sin(theta));
   }
 
   constexpr auto prob(T x) const {
-    return exp(-((x - mu_) * (x - mu_) / (2 * sigma_ * sigma_))) /
-           (sqrt(2 * std::numbers::pi) * sigma_);
+    using std::exp;
+    using std::sqrt;
+    return exp(-((x - mu_) * (x - mu_) / (2. * sigma_ * sigma_))) /
+           (sqrt(2. * std::numbers::pi) * sigma_);
   }
 
   constexpr auto logProb(T x) const {
-    return -((x - mu_) * (x - mu_) / (2 * sigma_ * sigma_)) -
-           0.5 * log(2 * std::numbers::pi) - log(sigma_);
+    using std::log;
+    return -((x - mu_) * (x - mu_) / (2. * sigma_ * sigma_)) -
+           (0.5 * log(2. * std::numbers::pi)) - log(sigma_);
   }
 
   constexpr auto cdf(T x) const {
-    0.5 * (1 + erf((x - mu_) / (sigma_ * sqrt(2))));
+    using std::erf;
+    0.5 * (1 + erf((x - mu_) / (sigma_ * std::numbers::sqrt2)));
   }
 
   constexpr auto mean() const { return mu_; }
