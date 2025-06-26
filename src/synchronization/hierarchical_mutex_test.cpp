4a23a4b7d901724d80abf9a7ecf8331a13daf3f9
Steven Ulin <steveulin@gmail.com>
2025-06-26 09:12:03 -0700

    Start a sychronization library

--- src/synchronization/hierarchical_mutex_test.cpp
+++ src/synchronization/hierarchical_mutex_test.cpp
@@ -2,20 +2,24 @@
 
 #include <mutex>
 
+#include "unit.h"
+
 using namespace tempura;
 
 auto main() -> int {
-  HierarchicalMutex mutex1{1};
-  HierarchicalMutex mutex2{2};
+  "HierarchicalMutex"_test = [] {
+    HierarchicalMutex mutex1{1};
+    HierarchicalMutex mutex2{2};
 
-  // Test locking and unlocking at different levels
-  mutex1.lock();
-  mutex2.lock();  // Should succeed, as level 2 > level 1
-  mutex2.unlock();
-  mutex1.unlock();
+    // Test locking and unlocking at different levels
+    mutex1.lock();
+    mutex2.lock();  // Should succeed, as level 2 > level 1
+    mutex2.unlock();
+    mutex1.unlock();
 
-  // Test trying to lock at a lower level
-  bool success = mutex1.try_lock();  // Should succeed
-  assert(success);
-  // mutex2.try_lock();        // Should fail, as level 1 < level 2
+    // Test trying to lock at a lower level
+    bool success = mutex1.try_lock();  // Should succeed
+    assert(success);
+    // mutex2.try_lock();        // Should fail, as level 1 < level 2
+  };
 }
